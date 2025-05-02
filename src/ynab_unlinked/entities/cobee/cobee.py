55c095e3ed10ce3fd366bebaf6e0638268f50137
Juanpe Araque <juanpedro.araque@datadoghq.com>
2025-05-02 18:43:50 +0200

    Fix cobee import to load dates properly

--- src/ynab_unlinked/entities/cobee/cobee.py
+++ src/ynab_unlinked/entities/cobee/cobee.py
@@ -1,17 +1,9 @@
-import re
 import datetime as dt
-from enum import Enum, auto
+import re
 from pathlib import Path
 
 from ynab_unlinked.context_object import YnabUnlinkedContext
 from ynab_unlinked.models import Transaction
-
-
-class ParseState(Enum):
-    DATE = auto()
-    PAYEE = auto()
-    AMOUNT = auto()
-
 
 DATE_REGEX = re.compile(r"(\d{1,2}) (\w{3}) (\d{4})")
 
@@ -49,20 +41,24 @@ def parse_date(date_str: str) -> dt.date | None:
 
 
 class Cobee:
-    def parse(self, input_file: Path, context: YnabUnlinkedContext) -> list[Transaction]:
+    def parse(
+        self, input_file: Path, context: YnabUnlinkedContext
+    ) -> list[Transaction]:
         # Import now the html parser
         import html_text
 
         text = html_text.extract_text(input_file.read_text())  # type: ignore
 
         start = False
-        state = ParseState.DATE
+        previous_line = ""
         transactions: list[Transaction] = []
         date: dt.date | None = None
         payee: str | None = None
         amount: float | None = None
 
         for line in text.splitlines():
+            line = line.strip()
+
             if not line:
                 continue
 
@@ -73,32 +69,35 @@ class Cobee:
             if not start:
                 continue
 
-            if state is ParseState.DATE:
-                if (date := parse_date(line.strip())) is None:
+            if (try_date := parse_date(line)) is not None:
+                date = try_date
+
+            if "€" in line:
+                amount_str = line.replace("€", "").replace(",", ".")
+
+                try:
+                    amount = float(amount_str)
+                except ValueError:
+                    # If we could not convert this to float it means this is not an amount line.
                     continue
 
-                state = ParseState.PAYEE
-                continue
+                # If it was the amount line, the previous line is the payee.
+                payee = previous_line
 
-            if state is ParseState.PAYEE:
-                payee = line.strip()
-                state = ParseState.AMOUNT
-                continue
-
-            if state is ParseState.AMOUNT:
-                amount_str = line.strip().replace("€", "").replace(",", ".")
-                if not amount_str:
-                    continue
-
-                amount = float(amount_str)
                 if date is None or payee is None:
                     raise ValueError(
                         f"The input file is not valid. The amount {amount} has been found without a date or payee."
                     )
 
                 transactions.append(Transaction(date=date, payee=payee, amount=amount))
+                continue
 
-                state = ParseState.DATE
+            if "Anulada" in line or "Rechazada" in line:
+                # These are transactions that didn't went through.
+                transactions.pop()
+                continue
+
+            previous_line = line
 
         return transactions
 
